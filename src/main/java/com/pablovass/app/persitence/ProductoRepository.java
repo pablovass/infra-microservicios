1fddaefe7629605be9028dc5bc7427ac4546305e
pablovass <faktory.fm@gmail.com>
2022-08-27 14:37:42 -0300

    repository

--- src/main/java/com/pablovass/app/persitence/ProductoRepository.java
+++ src/main/java/com/pablovass/app/persitence/ProductoRepository.java
@@ -2,10 +2,11 @@ package com.pablovass.app.persitence;
 
 import com.pablovass.app.persitence.crud.ProductoCrudRepository;
 import com.pablovass.app.persitence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
-
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     public List<Producto>getAll(){
@@ -16,6 +17,15 @@ public class ProductoRepository {
     }
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+    public Optional<Producto>getProducto(int idProducto){
+        return  productoCrudRepository.findById(idProducto);
+    }
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
+    }
+    public  Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
     }
 
 }
