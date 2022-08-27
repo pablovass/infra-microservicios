3632631540b8d2441458247c0173f66c4a5f753a
pablovass <faktory.fm@gmail.com>
2022-08-27 14:25:32 -0300

    creando query method

--- src/main/java/com/pablovass/app/persitence/ProductoRepository.java
+++ src/main/java/com/pablovass/app/persitence/ProductoRepository.java
@@ -4,10 +4,18 @@ import com.pablovass.app.persitence.crud.ProductoCrudRepository;
 import com.pablovass.app.persitence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     public List<Producto>getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
     }
+    public List<Producto>getByCategoria(int idCategoria){
+        return  productoCrudRepository.findByCategoriaOrderByNombreAsc(idCategoria);
+    }
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad,true);
+    }
+
 }
--- src/main/java/com/pablovass/app/persitence/crud/ProductoCrudRepository.java
+++ src/main/java/com/pablovass/app/persitence/crud/ProductoCrudRepository.java
@@ -1,7 +1,17 @@
 package com.pablovass.app.persitence.crud;
 
 import com.pablovass.app.persitence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+   // @Query(value = "SELECT* FROM productos  WHERE id_categoria=? ",nativeQuery = true)
+    //si tenemos la query podemos llama al metodo como nosotros queramos
+    List<Producto> findByCategoriaOrderByNombreAsc(int idCartegoria); // esto es un query method
+    Optional<List<Producto>>findByCantidadStockLessThanAndEstado(int cantidadStock,boolean estado);
+
+
 }
