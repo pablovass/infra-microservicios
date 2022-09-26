bcc55addff3526f15459e251f2410a54a3ff1e5a
pablovass <faktory.fm@gmail.com>
2022-09-26 10:54:15 -0300

    creacion del 1er projecto spring boot

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# spring-boot--app
-proyecto para crear una API Spring Boot
+# spring-boot- infra con java 8
+va ser una infraestructura de distintos microservicios conectados entre si 
+utilizando spring-boot y java 8 
---
+++ services-product /src/main/java/com/pablovass/ServicesProductApplication.java
@@ -0,0 +1,13 @@
+package com.pablovass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesProductApplication.class, args);
+	}
+
+}
