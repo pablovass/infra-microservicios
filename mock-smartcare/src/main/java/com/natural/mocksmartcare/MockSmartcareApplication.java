1cb06724241d8d9a6f3504c6f29fe078fecfac38
pablovass <faktory.fm@gmail.com>
2022-09-27 14:24:57 -0300

    mock de smartcare

---
+++ mock-smartcare/src/main/java/com/natural/mocksmartcare/MockSmartcareApplication.java
@@ -0,0 +1,13 @@
+package com.natural.mocksmartcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockSmartcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockSmartcareApplication.class, args);
+	}
+
+}
